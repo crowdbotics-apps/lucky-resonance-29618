02f2f1346388723ba7f574a043ee9ba3f73bd2e9
Crowdbotics <team@crowdbotics.com>
2021-08-07 19:13:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lucky_resonance_29618Identifier",
+  url: "https://lucky_resonance_29618Identifier.botics.co",
+  api: "https://lucky_resonance_29618Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
